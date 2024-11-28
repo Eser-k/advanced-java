75ddbeeaa4eace170834b0c97a7934e5aea2f1a0
Eser-k <eser.kayali@adesso.de>
2024-11-28 16:30:55 +0100

    Ways of getting the Class Object

--- HelloWorldMaven/src/main/java/testApp/App.java
+++ HelloWorldMaven/src/main/java/testApp/App.java
@@ -1,8 +1,30 @@
 package testApp;
 
+class User{
+	
+}
+
+class Employee extends User{
+	
+}
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws ClassNotFoundException {
+        
+    	var clazz1 = Employee.class;
+        
+        System.out.println(clazz1);
+        
+        var clazz2 = Class.forName("testApp.Employee");
+        
+        System.out.println(clazz2);
+
+        User u = new Employee();
+        
+        var clazz3 = u.getClass();
+        
+        System.out.println(clazz3);
+
+        
     }
 }
