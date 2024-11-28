2bc184f2d94372fcd58e4885b23128379ce9a910
Eser-k <eser.kayali@adesso.de>
2024-11-28 18:37:07 +0100

    Setting fields

--- HelloWorldMaven/src/main/java/testApp2/App.java
+++ HelloWorldMaven/src/main/java/testApp2/App.java
@@ -11,58 +11,66 @@ class User {
 class Employee extends User {
 	public String name;
 	private String password;
-	
+
 	private void calculateSalary() {
-		
+
 	}
-	
+
 	public boolean setUpdated(int sequence) {
-		
+
 		System.out.println("setUpdated: " + sequence);
-		
+
 		return true;
 	}
 }
 
 public class App {
-	public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, SecurityException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+	public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, SecurityException,
+			NoSuchMethodException, IllegalAccessException, InvocationTargetException {
 
 		var clazz = Employee.class;
 
 		// Only shows public Fields
 		System.out.println(Arrays.asList((clazz.getFields())));
-		
+
 		// Also shows private Fields
 		System.out.println(Arrays.asList((clazz.getDeclaredFields())));
-		
+
 		System.out.println();
-		
+
 		// Only shows public Methods
 		Arrays.asList(clazz.getMethods()).forEach(System.out::println);
-		
+
 		System.out.println();
 		System.out.println();
-		
+
 		// Also shows private Methods
 		Arrays.asList(clazz.getDeclaredMethods()).forEach(System.out::println);
-		
-		
-		var nameField = clazz.getField("name");
-		System.out.println(nameField);
-		
+
 		System.out.println();
 		System.out.println();
-		
+
+		// Checking if a method exists
 		var setUpdatedMethod = clazz.getDeclaredMethod("setUpdated", int.class);
 		System.out.println(setUpdatedMethod);
-		
+
 		var methodExists = Arrays.stream(clazz.getDeclaredMethods()).anyMatch(m -> m.getName().equals("setUpdated"));
 		System.out.println(methodExists);
-		
+
 		System.out.println();
 		System.out.println();
-		
+
+		// Calling a function
 		setUpdatedMethod.invoke(new Employee(), 5);
+
+		System.out.println();
+		System.out.println();
 		
+		// Setting Fields
+		var nameField = clazz.getField("name");
+		var user = new Employee();
+		nameField.set(user, "Pluto");
+		System.out.println("Name of the user: " + user.name);
+
 	}
 }
