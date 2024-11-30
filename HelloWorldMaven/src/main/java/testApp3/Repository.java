7e217d95c627b17f4586c4915ed860d6a48229f1
Eser-k <eser.kayali@adesso.de>
2024-11-30 13:26:04 +0100

    Getting attribute values

--- HelloWorldMaven/src/main/java/testApp3/Repository.java
+++ HelloWorldMaven/src/main/java/testApp3/Repository.java
@@ -9,12 +9,26 @@ public class Repository<T> {
 		
 		var fields = clazz.getDeclaredFields();
 		
+		for(var field : fields) {
+			
+			var annotations = field.getAnnotationsByType(Field.class);
+			if(annotations.length == 0) {
+				continue;
+			}
+			
+			var fieldName = annotations[0].value();
+			var isKey = annotations[0].isKey();
+			
+			System.out.println("fieldName: " + fieldName + ", " + "isKey: " + isKey);
+		}
+		
+		/*
 		var fieldList = Arrays
 			.stream(fields)
 			.filter(f -> f.getAnnotationsByType(Field.class).length > 0)
 			.collect(Collectors.toList());
 		
 		System.out.println(fieldList);
-		
+		*/
 	}
 }
